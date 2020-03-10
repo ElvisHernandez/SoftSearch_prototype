8cefe0289bee49b1e516c9d58b9257eb5635d225
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-10 04:41:43 -0400

    added dotenv gem and put API key in dotenv

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 
-const HelloWorld = () => {
+const HelloWorld = ({API_KEY}) => {
   const style = {
     width: "100%",
     height: "500px"
   };
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZWx2aXNoZXJuYW5kZXoiLCJhIjoiY2s3azJ5cHByMTJteTNsbWIwa2c2ZjR3YiJ9.FSjCSG-su1IiJqC4Hugp0Q';
+    mapboxgl.accessToken = API_KEY;
     var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
