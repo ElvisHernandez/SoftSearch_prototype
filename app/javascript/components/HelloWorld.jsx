369e2a88267e534616c6584ebb0b5b5bf7f4a392
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-10 04:54:52 -0400

    pass coords from map view to react components for custom search

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-const HelloWorld = ({API_KEY}) => {
+const HelloWorld = ({API_KEY, coords}) => {
   const style = {
     width: "100%",
     height: "500px"
@@ -11,11 +11,11 @@ const HelloWorld = ({API_KEY}) => {
     var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-80.1936589, 25.7742658],
+    center: coords,
     zoom:10
     });
     var marker = new mapboxgl.Marker()
-    .setLngLat([-80.1936589, 25.7742658])
+    .setLngLat(coords)
     .addTo(map);
   }, []);
 
