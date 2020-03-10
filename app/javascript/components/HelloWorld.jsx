271988202fc88f601ae2865697807e6a9967e409
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-10 09:02:22 -0400

    added turj cdn

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -6,22 +6,32 @@ const HelloWorld = ({API_KEY, coords}) => {
     height: "500px"
   };
 
+    var from = turf.point(coords);
+    var to = turf.point([-80.1373,26.1224]);
+    var options = {units: 'miles'};
+
+    var distance = turf.distance(from, to, options);
+
   useEffect(() => {
     mapboxgl.accessToken = API_KEY;
     var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
     center: coords,
-    zoom:10
+    zoom: 9
     });
     var marker = new mapboxgl.Marker()
     .setLngLat(coords)
     .addTo(map);
+
+    var ftlauderdale = new mapboxgl.Marker()
+    .setLngLat([-80.1373,26.1224])
+    .addTo(map);    
   }, []);
 
   return (
     <div>
-      <h1>Hello World!! React is back!</h1>
+      <h1>Hello World!! React is back! {distance}</h1>
       <div id="map" style={style}></div>
     </div>
   );
