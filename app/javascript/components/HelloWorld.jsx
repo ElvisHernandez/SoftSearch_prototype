21b4068ccf3bae9530e63f9c78825f7034a47434
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-10 03:25:22 -0400

    added mapbox cdn and sample map

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const HelloWorld = () => {
+
+    const style = {
+        width: "500px",
+        height: "500px"
+    }
+
+    useEffect(() => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZWx2aXNoZXJuYW5kZXoiLCJhIjoiY2s3a2JkMGViMTRlOTNlbXV4c3EwMXl4aiJ9.Qw2GhP9wSD3182I1Voj2lw';
+        var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11'
+        });
+    },[])
+
     return(
-        <h1>Hello World!! React is back!</h1>
+        <div>
+            <h1>Hello World!! React is back!</h1>
+            <div id='map' style={style}></div>
+        </div>
     )
 }
 
