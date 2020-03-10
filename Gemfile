7183180fbf4a2e67852cc11521a16155f64e048f
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-10 14:42:09 -0400

    added seed data with faker

--- Gemfile
+++ Gemfile
@@ -4,11 +4,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.0'
 
 gem 'dotenv-rails'
-
 gem 'geocoder'
 gem 'react-rails'
-
 gem 'faker'
+gem 'hirb'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+1000.times do 
+    Job.find_or_create_by(
+        position: Faker::Job.title,
+        description: Faker::Lorem.paragraph,
+        programming_language: Faker::ProgrammingLanguage.name,
+        date: Faker::Date.backward(days: 120),
+        longitude: Faker::Address.longitude,
+        latitude: Faker::Address.latitude
+    )
+end
