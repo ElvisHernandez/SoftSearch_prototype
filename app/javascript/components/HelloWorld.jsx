1888a7d7365ec432a8feca584180c6a2a3cf47a5
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-10 04:01:32 -0400

    centered map on miami with a marker

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -1,26 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 const HelloWorld = () => {
+  const style = {
+    width: "100%",
+    height: "500px"
+  };
 
-    const style = {
-        width: "500px",
-        height: "500px"
-    }
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZWx2aXNoZXJuYW5kZXoiLCJhIjoiY2s3azJ5cHByMTJteTNsbWIwa2c2ZjR3YiJ9.FSjCSG-su1IiJqC4Hugp0Q';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [-80.1936589, 25.7742658],
+    zoom:10
+    });
+    var marker = new mapboxgl.Marker()
+    .setLngLat([-80.1936589, 25.7742658])
+    .addTo(map);
+  }, []);
 
-    useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZWx2aXNoZXJuYW5kZXoiLCJhIjoiY2s3a2JkMGViMTRlOTNlbXV4c3EwMXl4aiJ9.Qw2GhP9wSD3182I1Voj2lw';
-        var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
-        });
-    },[])
+  return (
+    <div>
+      <h1>Hello World!! React is back!</h1>
+      <div id="map" style={style}></div>
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <h1>Hello World!! React is back!</h1>
-            <div id='map' style={style}></div>
-        </div>
-    )
-}
-
-export default HelloWorld
+export default HelloWorld;
